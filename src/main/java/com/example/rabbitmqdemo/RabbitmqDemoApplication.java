36136584e3e39577fffd84ab89270da586541e94
lihm1028 <sunshine1028@foxmail.com>
2022-06-03 11:03:08 +0800

    修改配置为localhost

--- src/main/java/com/example/rabbitmqdemo/RabbitmqDemoApplication.java
+++ src/main/java/com/example/rabbitmqdemo/RabbitmqDemoApplication.java
@@ -4,13 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@ServletComponentScan
 public class RabbitmqDemoApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqDemoApplication.class);
